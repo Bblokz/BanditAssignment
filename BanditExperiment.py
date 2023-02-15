0f952664db66484bbf5413eb4d8e74cfa29edc0a
arch_laptop <enricobonsu@hotmail.com>
2023-02-15 14:57:49 +0100

    Updated code

--- BanditExperiment.py
+++ BanditExperiment.py
@@ -18,16 +18,23 @@ def experiment(n_actions, n_timesteps, n_repetitions, smoothing_window):
     
     # Assignment 1: e-greedy
     env = BanditEnvironment(n_actions=n_actions)
-    pi = EgreedyPolicy(n_actions=n_actions)  # Initialize policy
-    for i in range(n_repetitions):
-        a = pi.select_action(epsilon=0.1)  # select action
-        r = env.act(a)  # sample reward
-        pi.update(a, r)  # update policy
-        # print("Test e-greedy policy with action {}, received reward {}".format(a, r))
+    # pi = EgreedyPolicy(n_actions=n_actions)  # Initialize policy
+    # for i in range(n_repetitions):
+    #     a = pi.select_action(epsilon=0.1)  # select action
+    #     r = env.act(a)  # sample reward
+    #     pi.update(a, r)  # update policy
+    #     # print("Test e-greedy policy with action {}, received reward {}".format(a, r))
 
     
     # Assignment 2: Optimistic init
-    
+    pi = OIPolicy(n_actions=n_actions,initial_value=1.0) # Initialize policy
+    for i in range(n_repetitions):
+        a = pi.select_action() # select action
+        r = env.act(a) # sample reward
+        pi.update(a,r) # update policy
+
+
+
     # Assignment 3: UCB
     
     # Assignment 4: Comparison
--- BanditPolicies.py
+++ BanditPolicies.py
@@ -10,6 +10,7 @@ By Thomas Moerland
 import numpy as np
 from BanditEnvironment import BanditEnvironment
 
+
 class EgreedyPolicy:
 
     def __init__(self, n_actions=10):
@@ -18,44 +19,49 @@ class EgreedyPolicy:
         self.estimates = np.zeros(n_actions)
         self.steps = np.zeros(n_actions)
         pass
-        
+
     def select_action(self, epsilon):
         # TO DO: Add own code
-        a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+        # Replace this with correct action selection
         # generate a random number between 0 and 1 and store it in rand using numpy
         # if rand is smaller than epsilon, select a random action
         # otherwise, select the greedy action
-        rand = np.random.uniform(0,1)
+        rand = np.random.uniform(0, 1)
         if rand < epsilon:
-            a = np.random.randint(0,self.n_actions)
+            a = np.random.randint(0, self.n_actions)
         else:
-            best_options = np.argmax(self.estimates)
-            a = best_options
+            a = np.argmax(self.estimates)
             print("Greedy: " + str(self.estimates) + " with action " + str(a))
 
         return a
-        
-    def update(self,a,r):
+
+    def update(self, a, r):
         # TO DO: Add own code
         self.steps[a] += 1
         self.estimates[a] += (1/self.steps[a])*(r-self.estimates[a])
         pass
+
 
 class OIPolicy:
 
     def __init__(self, n_actions=10, initial_value=0.0, learning_rate=0.1):
         self.n_actions = n_actions
         # TO DO: Add own code
+        self.estimates = np.zeros(n_actions)
+        self.learning_rate = learning_rate
         pass
-        
+
     def select_action(self):
         # TO DO: Add own code
-        a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+        # a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+        a = np.argmax(self.estimates)
         return a
-        
-    def update(self,a,r):
+
+    def update(self, a, r):
         # TO DO: Add own code
+        self.estimates[a] += self.learning_rate * (r-self.estimates[a])
         pass
+
 
 class UCBPolicy:
 
@@ -63,38 +69,41 @@ class UCBPolicy:
         self.n_actions = n_actions
         # TO DO: Add own code
         pass
-    
+
     def select_action(self, c, t):
         # TO DO: Add own code
-        a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+        # Replace this with correct action selection
+        a = np.random.randint(0, self.n_actions)
         return a
-        
-    def update(self,a,r):
+
+    def update(self, a, r):
         # TO DO: Add own code
         pass
-    
+
+
 def test():
 
     n_actions = 10
-    env = BanditEnvironment(n_actions=n_actions) # Initialize environment    
-    
-    pi = EgreedyPolicy(n_actions=n_actions) # Initialize policy
-    a = pi.select_action(epsilon=0.5) # select action
-    r = env.act(a) # sample reward
-    pi.update(a,r) # update policy
-    print("Test e-greedy policy with action {}, received reward {}".format(a,r))
-    
-    pi = OIPolicy(n_actions=n_actions,initial_value=1.0) # Initialize policy
-    a = pi.select_action() # select action
-    r = env.act(a) # sample reward
-    pi.update(a,r) # update policy
-    print("Test greedy optimistic initialization policy with action {}, received reward {}".format(a,r))
-    
-    pi = UCBPolicy(n_actions=n_actions) # Initialize policy
-    a = pi.select_action(c=1.0,t=1) # select action
-    r = env.act(a) # sample reward
-    pi.update(a,r) # update policy
-    print("Test UCB policy with action {}, received reward {}".format(a,r))
-    
+    env = BanditEnvironment(n_actions=n_actions)  # Initialize environment
+
+    pi = EgreedyPolicy(n_actions=n_actions)  # Initialize policy
+    a = pi.select_action(epsilon=0.5)  # select action
+    r = env.act(a)  # sample reward
+    pi.update(a, r)  # update policy
+    print("Test e-greedy policy with action {}, received reward {}".format(a, r))
+
+    pi = OIPolicy(n_actions=n_actions, initial_value=1.0)  # Initialize policy
+    a = pi.select_action()  # select action
+    r = env.act(a)  # sample reward
+    pi.update(a, r)  # update policy
+    print("Test greedy optimistic initialization policy with action {}, received reward {}".format(a, r))
+
+    pi = UCBPolicy(n_actions=n_actions)  # Initialize policy
+    a = pi.select_action(c=1.0, t=1)  # select action
+    r = env.act(a)  # sample reward
+    pi.update(a, r)  # update policy
+    print("Test UCB policy with action {}, received reward {}".format(a, r))
+
+
 if __name__ == '__main__':
     test()
