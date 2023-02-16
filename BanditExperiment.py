17c6e6279eb4a62feefcffd1b9ff68e278c670b7
arch_laptop <enricobonsu@hotmail.com>
2023-02-16 14:13:29 +0100

    Updated UCB

--- BanditExperiment.py
+++ BanditExperiment.py
@@ -11,11 +11,11 @@ import numpy as np
 from BanditEnvironment import BanditEnvironment
 from BanditPolicies import EgreedyPolicy, OIPolicy, UCBPolicy
 from Helper import LearningCurvePlot, ComparisonPlot, smooth
- 
+
 
 def experiment(n_actions, n_timesteps, n_repetitions, smoothing_window):
-    #To Do: Write all your experiment code here
-    
+    # To Do: Write all your experiment code here
+
     # Assignment 1: e-greedy
     env = BanditEnvironment(n_actions=n_actions)
     # pi = EgreedyPolicy(n_actions=n_actions)  # Initialize policy
@@ -25,28 +25,33 @@ def experiment(n_actions, n_timesteps, n_repetitions, smoothing_window):
     #     pi.update(a, r)  # update policy
     #     # print("Test e-greedy policy with action {}, received reward {}".format(a, r))
 
-    
     # Assignment 2: Optimistic init
-    pi = OIPolicy(n_actions=n_actions,initial_value=1.0) # Initialize policy
-    for i in range(n_repetitions):
-        a = pi.select_action() # select action
-        r = env.act(a) # sample reward
-        pi.update(a,r) # update policy
-
-
+    # pi = OIPolicy(n_actions=n_actions,initial_value=1.0) # Initialize policy
+    # for i in range(n_repetitions):
+    #     a = pi.select_action() # select action
+    #     r = env.act(a) # sample reward
+    #     pi.update(a,r) # update policy
 
     # Assignment 3: UCB
-    
+    pi = UCBPolicy(n_actions=n_actions)  # Initialize policy
+
+    for i in range(1, n_repetitions):
+        a = pi.select_action(c=1.0, t=i)  # select action
+        r = env.act(a)  # sample reward
+        pi.update(a, r)  # update policy
+
+    print("Test UCB policy with action {}, received reward {}".format(a, r))
     # Assignment 4: Comparison
-    
+
     pass
+
 
 if __name__ == '__main__':
     # experiment settings
-    n_actions = 10
-    n_repetitions = 500
+    n_actions = 3
+    n_repetitions = 1000
     n_timesteps = 1000
     smoothing_window = 31
-    
-    experiment(n_actions=n_actions,n_timesteps=n_timesteps,
-               n_repetitions=n_repetitions,smoothing_window=smoothing_window)
+
+    experiment(n_actions=n_actions, n_timesteps=n_timesteps,
+               n_repetitions=n_repetitions, smoothing_window=smoothing_window)
--- BanditPolicies.py
+++ BanditPolicies.py
@@ -68,14 +68,20 @@ class UCBPolicy:
         self.n_actions = n_actions
         self.estimates = np.zeros(n_actions)
         self.steps = np.zeros(n_actions)
-        self.steps += 1e-5
+        self.steps += 1e-3
+        print(self.steps)
+
 
         # TO DO: Add own code
         pass
 
     def select_action(self, c, t):
         # make sure we do not divide by zero
-        return np.argmax(self.estimates + c * np.sqrt(np.log(t) / (self.steps)))
+        x = np.argmax(self.estimates + c * np.sqrt(np.log(t) / (self.steps)))
+        # print(c * np.sqrt(np.log(t) / (self.steps)))
+        print(self.estimates + c * np.sqrt(np.log(t) / (self.steps)))
+        print("Picked action", str(x))
+        return x
 
     def update(self, a, r):
         self.steps[a] += 1
