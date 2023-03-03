d886aa3215358e880c7a40de2a669013e103a264
FurryOstrich <79595891+FurryOstrich@users.noreply.github.com>
2023-03-03 17:49:18 +0100

    code cleanup

--- BanditExperiment.py
+++ BanditExperiment.py
@@ -14,66 +14,6 @@ from Helper import LearningCurvePlot, ComparisonPlot, smooth
 
 
 def experiment(n_actions, n_timesteps, n_repetitions, smoothing_window):
-    # To Do: Write all your experiment code here
-
-    # env = BanditEnvironment(n_actions=n_actions)
-    # Assignment 1: egreedy
-    # eHyper = [0.01, 0.05, 0.1, 0.25]
-    # sumResultEGreedy = np.zeros(len(eHyper))
-    # plotHelper = LearningCurvePlot(
-    #     title="Average performance e-greedy over {} repetitions".format(n_repetitions))
-
-    # for index, e in enumerate(eHyper):
-    #     print("running e-greedy value " + str(e))
-    #     result = run_egreedy(n_actions, n_timesteps, n_repetitions, e)
-    #     # average of result epsilon over n_timesteps.
-    #     sumResultEGreedy[index] = sum(result)/(n_timesteps * n_repetitions)
-    #     plotHelper.add_curve(
-    #         smooth(result/float(n_repetitions), window=smoothing_window), label='e-greedy value ' + str(e))
-
-    # plotHelper.save("egreedy.png")
-
-    # # Assignment 2: Optimistic Initialization
-    # initHyper = [0.1, 0.5, 1.0, 2.0]
-    # sumResultOI = np.zeros(len(initHyper))
-    # plotHelper = LearningCurvePlot(
-    #     title="Average performance Optimistic Initialization values over {} repetitions".format(n_repetitions))
-
-    # for index, init in enumerate(initHyper):
-    #     print("running OI value " + str(init))
-    #     result = run_OI(n_actions, n_timesteps, n_repetitions, 0.1, init)
-    #     sumResultOI[index] = sum(result)/(n_timesteps * n_repetitions)
-    #     plotHelper.add_curve(
-    #         smooth(result/float(n_repetitions), window=smoothing_window), label='OI value ' + str(init))
-
-    # plotHelper.save("OI.png")
-
-    # # Assignment 3: Upper Confidence Bounds
-    # cHyper = [0.01, 0.05, 0.1, 0.25, 0.5, 1.0]
-
-    # sumResultUCB = np.zeros(len(cHyper))
-
-    # plotHelper = LearningCurvePlot(
-    #     title="Average performance Upper Confidence Bounds over {} repetitions".format(n_repetitions))
-
-    # for index, c in enumerate(cHyper):
-    #     print("running UCB value " + str(c))
-    #     result = run_ucb(n_actions, n_timesteps, n_repetitions, c)
-    #     sumResultUCB[index] = sum(result)/(n_timesteps * n_repetitions)
-    #     plotHelper.add_curve(
-    #         smooth(result/float(n_repetitions), window=smoothing_window), label='UCB value ' + str(c))
-
-    # plotHelper.save("UCB.png")
-
-    # # Assignment 4: Comparison
-    # plotHelper = ComparisonPlot(
-    #     title="Average reward algorithms over {} repetitions".format(n_repetitions))
-
-    # plotHelper.add_curve(eHyper, sumResultEGreedy, label='e-greedy')
-    # plotHelper.add_curve(initHyper, sumResultOI, label='OI')
-    # plotHelper.add_curve(cHyper, sumResultUCB, label='UCB')
-
-    # plotHelper.save("Compare.png")
 
     # Bonus: Gradiant Bandit
     gLearningHyper = [0.01, 0.05, 0.1, 0.25, 0.5, 1.0]
